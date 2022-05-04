ffcfeeab293be8853d12e4caed96e6b5883e6d18
Anzhelav <vasianzh@gmail.com>
2022-05-03 21:27:09 -0500

    First mentoring

---
+++ src/GitCommands/GitCommands1.java
@@ -0,0 +1,34 @@
+package GitCommands;
+
+public class GitCommands1 {
+    public static void main(String[] args) {
+        //Basic Git Commands:
+        //Git: configurations
+        //$ git config --global user.name "FirstName LastName"
+        //$ git config-global user.email "your-email@email-provider.com
+        //$ git config-global color.ui true
+        //$ git config--ist
+        //Git: starting a repository
+        //§ git init
+        //§ git status
+        //Git: staging files
+        //$ git add «file-name›
+        //§ git add .
+        //$ git rm --cached «file-name>
+        //§ git reset «file-name›
+        //Git: committing to a repository
+        //$ git commit -m "Add three files"
+        //$ git commit --amend -m ‹enter your message>
+        //Git: pulling and pushing from and to repositories
+        //$ git remote add origin «link>
+        //§ git push -u origin master
+        //$ git clone «clone>
+        //§ git pull
+        //Git: branching
+        //$ git branch
+        //§ git branch «branch-name>
+        //$ git checkout «branch-name>
+        //$ git merge «branch-name>
+        //$ git checkout -b ‹branch-name>
+    }
+}
